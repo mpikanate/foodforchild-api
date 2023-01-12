4d5ca5f96c5179914b6d71ede9414004eb5ff24e
Pikanate M <pikanate.mak@fullteam.tech>
2023-01-12 15:17:22 +0700

    allow cors

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ import express from "express";
 import http from "http";
 import * as routes from "./routes/index.js"
 import * as Route from "./services/routing.service.js";
+import cors from "cors"
 
 const host = process.env.SERVER_HOST;
 const port = process.env.SERVER_PORT;
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 
 // Routing
 Route.endpointsInit(app);
