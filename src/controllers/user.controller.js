fc300b40fb9cc46896925300adc37b15802e50f0
Pikanate M <pikanate.mak@fullteam.tech>
2023-01-12 11:35:51 +0700

    add function login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,6 +16,19 @@ export const create = async (req, res) => {
 	}
 };
 
+export const login = async (req, res) => {
+	try {
+		logger("info", "===== User Create =====");
+		const dataObject = {
+			
+		};
+		return customResp(res, 200, `SUCCESS`, dataObject);
+
+	} catch (e) {
+		return customResp(res, 500, `SOMETHING_WENT_WRONG`, e.message);
+	}
+};
+
 export default {
 	create
 };
